5257ed35d384415c8c02371740da87e05d80a29d
Edgarlct <90525103+Edgarlct@users.noreply.github.com>
2021-11-23 18:44:58 +0100

    :hammer: [upadte] create search bar

--- components/search-event.js
+++ components/search-event.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import {View, Text, Image, TextInput, StyleSheet} from 'react-native';
-import {autoCapitalize} from "react-native/Libraries/DeprecatedPropTypes/DeprecatedTextInputPropTypes";
+import {View, Image, TextInput, StyleSheet} from 'react-native';
 
 class SearchEvent extends React.Component {
     render() {
         return (
-            <View>
+            <View style={styles.searchBar}>
+                <Image style={styles.iconSearch} source={require('../assets/search.png')}/>
                 <TextInput
-                    style={styles.input}
-                    placeholder={"Rechercher"}
+                    placeholder="Titre de l'événement"
+                    // onChangeText={(text) => this._searchTextInputChanged(text)}
+                    returnKeyType= 'search' // met le bouton de retour en mode recherche
+                    // onSubmitEditing={() => this._searchFilm()}
                 />
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
-    input:{
-        height: 49,
+    searchBar:{
+        borderRadius: 50,
+        paddingLeft: 15,
+        width: 'auto',
+        backgroundColor: '#ffffff',
+        height: 50,
+        justifyContent: "flex-start",
+        marginBottom: 24,
+        flexDirection: "row",
+        alignItems: "center",
+
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+        borderColor: "#EEEEEE",
+        borderWidth: 1,
+    },
+
+    iconSearch:{
+        marginRight: 15,
     }
+
 })
 
 
-export default EventCard;
+export default SearchEvent;
